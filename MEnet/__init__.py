d189366b925f6c5f8c56c4bd7ff7267ad2af2fda
yyoshiaki <y.yasumizu@gmail.com>
2021-03-18 13:06:44 +0900

    enable pip install

--- MEnet/__init__.py
+++ MEnet/__init__.py
@@ -1,4 +1,6 @@
-# from MEnet.model import *
-# from MEnet.utils import *
-# from MEnet.train import *
-# from MEnet.predict import *
+from .models import *
+from .utils import *
+from .train import *
+from .predict import *
+
+__version__ = '0.0.1'
