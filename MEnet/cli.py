c92d33722c3160ffc32de3e5c968de11a561636d
yyoshiaki <y.yasumizu@gmail.com>
2021-11-05 12:19:53 +0900

    tile

--- MEnet/cli.py
+++ MEnet/cli.py
@@ -5,9 +5,11 @@ import argparse
 import MEnet
 
 
-def command_preprocess(args):
-    print(args)
+def command_tile(args):
+    from .tile import tile
 
+    print(args)
+    tile(args)
 
 def command_train(args):
     from .train import train
@@ -30,24 +32,10 @@ def main():
     print_logo()
     print('MEnet version : ', MEnet._version.__version__)
 
-    parser = argparse.ArgumentParser(description='MEnet')
+    parser = argparse.ArgumentParser(description='MEnet : Neural net based cell deconvolution utilizing DNA methylation.\n\n'
+    'Developer : Yoshiaki Yasumizu, Yuto Umedu (Osaka University)\n'
+    'Licence : CC BY-NC 4.0')
     subparsers = parser.add_subparsers()
-
-    parser_preprocess = subparsers.add_parser(
-        'preprocess', help='see `preprocess -h`')
-    parser_preprocess.add_argument(
-        '-i', '--input', metavar='input', help='input')
-    parser_preprocess.add_argument(
-        '-r', '--reference', metavar='reference', help='reference')
-    parser_preprocess.set_defaults(handler=command_train)
-
-    # train
-    parser_train = subparsers.add_parser('train', help='see `train -h`')
-    parser_train.add_argument(
-        'input_yaml', action='store', help='input yaml file.')
-    parser_train.add_argument(
-        '--device', type=str, default=None, help='device for pytorch. (ex. cpu, cuda)')
-    parser_train.set_defaults(handler=command_train)
 
     # predict
     parser_predict = subparsers.add_parser('predict', help='see `predict -h`')
@@ -68,6 +56,26 @@ def main():
     # parser_predict.add_argument('--device', type=str, default=None, help='device for pytorch. (ex. cpu, cuda)')
 
     parser_predict.set_defaults(handler=command_predict)
+
+    # tile 
+    parser_tile = subparsers.add_parser(
+        'tile', help='see `tile -h`', description="Tile bismark.cov beforehand. "
+                "This function is useful for making predictions using various models for a single Bismark file.")
+    parser_tile.add_argument(
+        'input', action='store', help='input bismark file.')
+    parser_tile.add_argument(
+        '-b', '--bp', metavar='bp', type=int, help='tile size (bp)', required=True)
+    parser_tile.add_argument(
+        '--bedtools', type=str, default='bedtools', help='Full path to bedtools.')
+    parser_tile.set_defaults(handler=command_tile)
+
+    # train
+    parser_train = subparsers.add_parser('train', help='see `train -h`')
+    parser_train.add_argument(
+        'input_yaml', action='store', help='input yaml file.')
+    parser_train.add_argument(
+        '--device', type=str, default=None, help='device for pytorch. (ex. cpu, cuda)')
+    parser_train.set_defaults(handler=command_train)
 
     parser_help = subparsers.add_parser('help', help='see `help`')
     parser_help.add_argument(
---
+++ MEnet/tile.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from MEnet import utils, _version
+import os
+import argparse
+
+def tile(args):
+    f_input = args.input
+    tile_bp = args.bp
+
+    print(f_input, tile_bp)
+    if os.path.exists('{n}.tile{t}bp.csv'.format(n=f_input.split('.bis')[0], t=tile_bp)):
+        print('Input file has already been tiled. If you need to tile it again, delete the existing file ({}).'.format(
+            '{n}.tile{t}bp.csv'.format(n=f_input.split('.bis')[0], t=tile_bp)))
+        return
+    else:
+        print('Tiling bismark cov...')
+        df_input = utils.tile_bismark(f_input, tile_bp, args.bedtools)
+        df_input.to_csv(
+            '{n}.tile{t}bp.csv'.format(n=f_input.split('.bis')[0], t=tile_bp))
+
+        return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.input = 'test/predict/Minion_STR1_Fr6.bis.cov.gz'
+    args.bp = 500
+
+    tile(args)
